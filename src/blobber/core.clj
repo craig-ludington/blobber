bf3981e1cd67d7548ba85f585e8ea4beee50e4ef
Craig Ludington <me@alpheus.me>
2012-08-11 20:38:18 -0500

    Add wrap-stacktrace so we get pretty html stacktraces.

--- src/blobber/core.clj
+++ src/blobber/core.clj
@@ -1,6 +1,6 @@
 (ns blobber.core
   (:use [compojure.core :only [defroutes GET POST DELETE ANY]]
-        [ring.middleware reload])
+        [ring.middleware reload stacktrace])
   (:require [blobber.config :as config]
             [compojure.route :as route]
             [compojure.handler :as handler]
@@ -10,14 +10,15 @@
             [blobber.storage :as storage]))
 
 (defroutes routes
-  (GET    "/"        []             "<h1>I'm Blobber.  Go away.</h1>")
+  (GET    "/"        []             "<h1>I'm Blobber.</h1>")
   (GET    ["/:uuid"] [uuid]         (storage/fetch uuid))
   (DELETE ["/:uuid"] [uuid]         (storage/delete uuid))
   (POST   "/"        { body :body } (storage/create (slurp body)))
   (ANY    "*"        []             "<h1>Page not found.</h1>"))
 
 (def application (-> (handler/site routes)
-                     (wrap-reload '(blobber.core))))
+                     (wrap-reload '(blobber.core))
+                     (wrap-stacktrace)))
 
 (defn start []
   (ring/run-jetty #'application {:port (or config/port 8080) :join? false}))
