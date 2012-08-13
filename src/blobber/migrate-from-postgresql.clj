7697b3d1382002554ddcdac21ad3b1412c5cb635
Craig Ludington <me@alpheus.me>
2012-08-12 22:44:07 -0500

    Make migrate-from-postgresql work with the test database.
    * Worked the first time!
    
    * Needs parameterization for
    ** table
    ** number of rows to process
    ** blob column
    ** hash column
    ** delete blob column on hash column update

--- src/blobber/migrate-from-postgresql.clj
+++ src/blobber/migrate-from-postgresql.clj
@@ -1,15 +1,21 @@
 (ns blobber.migrate-from-postgresql
   (:require [blobber.config    :as config]
-            [clojure.java.jdbc :as sql]))
+            [clojure.java.jdbc :as sql]
+            [blobber.storage   :as storage]))
 
-(defn migrate-blob []
+(defn migrate-blob [result]
+  (println (str result))
   (sql/with-connection (config/db :admin)
-    ;; TODO 
-    ))
+    (sql/transaction
+     (let [key (storage/create (:blob result))]
+       (sql/update-values :blobs ["id = ?" (:id result)] {:hash key :blob nil} )))))
 
 (defn migrate-blobs []
-  ;; TODO
-  )
+  (sql/with-connection (config/db :admin)
+    (sql/with-query-results
+      results
+      ["SELECT id, blob FROM blobs WHERE hash IS NULL AND blob IS NOT NULL ORDER BY id ASC LIMIT 10"]
+      (doseq [result results] (migrate-blob result)))))
 
 (defn -main []
   (print " ...") (flush)
