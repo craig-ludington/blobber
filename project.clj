8bca5b9da442534f3d029ec1dbf964204dc58687
Craig Ludington <me@alpheus.me>
2012-08-14 23:48:21 -0500

    Add :ring to project.clj so we can make a WAR file.
    N.B. This requires adding [lein-ring "0.7.2"] to :plugins in ~/.lein/profiles.clj.
    It could have alternatively been added to the blobber project.clj.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject blobber "0.1.0-SNAPSHOT"
   :description "Store, fetch, and delete binary large objects (BLOB) in PostgreSQL."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/craig-ludington/blobber"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.1"]
                  [ring/ring-devel "1.1.1"]
                  [compojure "1.1.1"]
-                 [filesystem-trie "0.1.0-SNAPSHOT"]])
+                 [filesystem-trie "0.1.0-SNAPSHOT"]]
+  :ring {:handler blobber.core/application} )
