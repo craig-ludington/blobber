2e6926836bf6652a847cc7d69c9e3fcc5090b027
Craig Ludington <alpheus@rimpoche.chi.il.us>
2012-10-16 16:32:59 -0500

    Test public functions in blobber.password

--- test/blobber/password_test.clj
+++ test/blobber/password_test.clj
@@ -12,7 +12,35 @@
                           (throw (Throwable. (str e ".  Can't create temp file in /tmp."))))))
 
 (deftest add-user-test
-  (let [x (blobber.password/add-user "alice" "secret" ( password-file))]
+  (let [x (blobber.password/add-user "alice" "secret" (password-file))]
     (is (= (class x) java.lang.String))
     (is (= 2 (count (str/split x #":"))))
-    ))
+    (is (= "alice" (first (str/split x #":"))))))
+
+(deftest add-existing-user-should-fail-test
+  (let [file (password-file)]
+    (blobber.password/add-user "alice" "secret" file)
+    (is (= (str "Cannot add existing user \"alice\" to password file \"" file "\".")
+           (try (blobber.password/add-user "alice" "fish" file)
+                                  (catch Throwable e
+                                    (.getMessage e)))))))
+
+(deftest remove-user-test
+  (let [file (password-file)]
+    (blobber.password/add-user "alice" "secret" file)
+    (blobber.password/add-user "bob" "tangent" file)
+    (blobber.password/remove-user "alice" file)
+    (is (not (check-user-password "alice" "secret" file)))
+    (is (check-user-password "bob" "tangent" file))))
+
+(deftest remove-non-existent-user-should-fail-test
+  (let [file (password-file)]
+    (is (= (str "Cannot remove non-existent user \"alice\" from password file \"" file "\".")
+           (try (blobber.password/remove-user "alice" file)
+                                  (catch Throwable e
+                                    (.getMessage e)))))))
+(deftest check-user-password-test
+  (let [file (password-file)]
+    (blobber.password/add-user "alice" "secret" file)
+    (is (check-user-password "alice" "secret" file))
+    (is (not (check-user-password "alice" "Secret" file)))))
